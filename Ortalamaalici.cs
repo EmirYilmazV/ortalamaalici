a118dd375734f235f0237ef6777b84c146506146
EmirYilmazV <emiryilmaz2505@gmail.com>
2024-11-20 22:53:18 +0300

    Create Ortalamaalici.cs

---
+++ Ortalamaalici.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+
+{
+    static void Main()
+    {
+        Console.WriteLine("Lütfen ilk dersin notunu giriniz");
+        int not1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Lütfen ikinci dersin notunu giriniz");
+        int not2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Lütfen Üçüncü Dersin notunu giriniz");
+        int not3 = Convert.ToInt32(Console.ReadLine());
+
+        double ort = (not1 + not2 + not3) / 3;
+        
+        if (ort < 50)
+
+        Console.WriteLine("Sinifta Kaldiniz Ortalamaniz: {0}", ort);
+
+        else if (ort > 50)
+
+        Console.WriteLine("Sinifi Geçtiniz Ortalamaniz: {0}", ort);
+
+    
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
